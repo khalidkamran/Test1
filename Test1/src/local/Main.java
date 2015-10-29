32627e182ed93ea1444a60dbd4a58c9e9c027000
Kamran Khalid <khalidkamran@hotmail.com>
2015-10-30 02:53:01 +0500

    Template project

---
+++ Test1/src/local/Main.java
@@ -0,0 +1,11 @@
+package local;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is the template project");
+	}
+
+}
