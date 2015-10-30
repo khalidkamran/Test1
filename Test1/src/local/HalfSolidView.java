1cd3f34bef7bb6bc608daa635d89908208745ab9
Kamran Khalid <khalidkamran@hotmail.com>
2015-10-31 01:03:50 +0500

    extra solid view

--- Test1/src/local/HalfSolidView.java
+++ Test1/src/local/HalfSolidView.java
@@ -5,6 +5,7 @@ public class HalfSolidView implements ViewResolver{
 	@Override
 	public void getView() {
 		System.out.println("This is half solid view");
+		System.out.println("This is Extra half solid view");
 		
 	}
 
